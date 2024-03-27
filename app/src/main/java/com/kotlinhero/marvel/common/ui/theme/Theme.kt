76b2ac7fd3f840d2ea084453b0cc2f69bd269fa6
KotlinHero <ahmad.alshahal2@gmail.com>
2024-03-27 18:32:12 +0300

    feat
    Added Gilroy FontFamily

--- app/src/main/java/com/kotlinhero/marvel/common/ui/theme/Theme.kt
+++ app/src/main/java/com/kotlinhero/marvel/common/ui/theme/Theme.kt
@@ -3,13 +3,26 @@ package com.kotlinhero.marvel.common.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.kotlinhero.marvel.R
 
 private val ColorScheme = lightColorScheme(
     primary = Red,
     background = White,
     onBackground = DarkGray,
     secondary = Gray
+)
+
+val Gilroy = FontFamily(
+    Font(R.font.gilroy_black, FontWeight.Black),
+    Font(R.font.gilroy_bold, FontWeight.Bold),
+    Font(R.font.gilroy_light, FontWeight.Light),
+    Font(R.font.gilroy_medium, FontWeight.Medium),
+    Font(R.font.gilroy_regular, FontWeight.Normal),
+    Font(R.font.gilroy_semi_bold, FontWeight.SemiBold),
+    Font(R.font.gilroy_extra_bold, FontWeight.ExtraBold),
 )
 
 @Composable
--- app/src/main/java/com/kotlinhero/marvel/common/ui/theme/Type.kt
+++ app/src/main/java/com/kotlinhero/marvel/common/ui/theme/Type.kt
@@ -1,34 +1,28 @@
 package com.kotlinhero.marvel.common.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+private val defaultTypography = Typography()
+private val defaultFontFamily = Gilroy
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = defaultFontFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = defaultFontFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = defaultFontFamily),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = defaultFontFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = defaultFontFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = defaultFontFamily),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = defaultFontFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = defaultFontFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = defaultFontFamily),
+
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = defaultFontFamily),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = defaultFontFamily),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = defaultFontFamily),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = defaultFontFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = defaultFontFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = defaultFontFamily)
 )
