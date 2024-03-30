e7d1655a0d5ea3824cf9b0bd39811f208df18ec2
KotlinHero <ahmad.alshahal2@gmail.com>
2024-03-30 16:50:51 +0300

    refactor

--- app/src/main/java/com/kotlinhero/marvel/main/ui/MainActivity.kt
+++ app/src/main/java/com/kotlinhero/marvel/main/ui/MainActivity.kt
@@ -2,14 +2,13 @@ package com.kotlinhero.marvel.main.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.kotlinhero.marvel.core.ui.navigation.Destination
 import com.kotlinhero.marvel.core.ui.providers.LocalNavController
+import com.kotlinhero.marvel.core.utils.enableDarkEdgeToEdge
 import com.kotlinhero.marvel.main.ui.navigation.MarvelNavHost
 import com.kotlinhero.marvel.main.ui.theme.MarvelTheme
 import com.kotlinhero.marvel.main.ui.viewmodels.MainViewModel
@@ -25,14 +24,7 @@ class MainActivity : ComponentActivity() {
             viewModel.splashScreenVisible
         }
 
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(
-                android.graphics.Color.TRANSPARENT,
-            ),
-            navigationBarStyle = SystemBarStyle.dark(
-                android.graphics.Color.TRANSPARENT,
-            )
-        )
+        enableDarkEdgeToEdge()
 
         setContent {
             MarvelTheme {
